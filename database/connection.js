03cc907a918b6b29e423b141d75a0aa1e6f01b88
melodyulloa <50853884+melodyulloa@users.noreply.github.com>
2019-12-04 19:28:58 -0500

    preparing for heroku deployment

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,22 @@
 const Sequelize = require("sequelize");
 
-const sequelize= new Sequelize("simplepos_db","root","password",{
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    operatorsAliases: false
-});
+var sequelize;
+
+if(process.env.DATABASE_URL){
+     sequelize= new Sequelize(process.env.DATABASE_URL,{
+        dialect: 'postgres',
+        protocol:'postgres',
+        port:'5432',
+        host:'ec2-174-129-255-4.compute-1.amazonaws.com',
+        logging: true
+    });
+}else{ // development only
+    sequelize= new Sequelize("simplepos_db","root","password",{
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        operatorsAliases: false
+    });
+}
 
 module.exports = sequelize;
 global.sequelize = sequelize;
