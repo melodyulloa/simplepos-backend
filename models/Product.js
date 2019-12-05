c29e43deae0016e5e5cc4f14839b8739a802531d
melodyulloa <50853884+melodyulloa@users.noreply.github.com>
2019-12-04 22:02:57 -0500

    fix sequelize error

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 const Sequelize=require("sequelize");
 
 
-module.exports = sequelize.define("Product",{
+module.exports = sequelize.define("product",{
       id:{
         type:Sequelize.INTEGER(11),
         allowNull:false,
