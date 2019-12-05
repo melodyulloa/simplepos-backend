006e9c3914c5203cd3bc538b41f479c09bd3b984
melodyulloa <50853884+melodyulloa@users.noreply.github.com>
2019-12-04 21:59:17 -0500

    testing

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const Sequelize=require("sequelize");
 
 
-module.exports = sequelize.define('User',{
+module.exports = sequelize.define('user',{
   id:{
     type:Sequelize.INTEGER(11),
     allowNull:false,
